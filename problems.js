a5dc7f6f07b06c0322933254dc77cf3df17dff45
Mitchell Hudson <soggybag@gmail.com>
2024-01-22 12:52:06 -0800

    Create problems.js

---
+++ problems.js
@@ -0,0 +1,50 @@
+// Use the following snippet to test your code before submitting
+export const runner = () => {
+  const array = [1, 2, 3, 4, 5];
+  sumArray(array);
+};
+
+/*
+  Exercise 1/5
+  Use array.reduce to return the sum of all the numbers in the array
+  The function should return 0 if the array is empty 
+*/
+export function sumArray(array) {
+  throw new Error("Not implemented.");
+}
+
+/* 
+  Exercise 2/5
+  Use array.reduce to return the product of all the numbers in the array
+  The function should return 1 if the array is empty 
+*/
+export function productArray(array) {
+  throw new Error("Not implemented.");
+}
+
+/*
+  Exercise 3/5
+  Use array.reduce to return the largest number in the array
+  The function should return -Infinity if the array is empty
+*/
+export function maxArray(array) {
+  throw new Error("Not implemented.");
+}
+
+/*
+  Exercise 4/5
+  Use array.reduce to return the smallest number in the array
+  The function should return Infinity if the array is empty
+*/
+export function minArray(array) {
+  throw new Error("Not implemented.");
+}
+
+/* 
+  Exercise 5/5
+  Use array.reduce to return the average of all the numbers in the array
+  The function should return 0 if the array is empty
+*/
+export function averageArray(array) {
+  throw new Error("Not implemented.");
+}
